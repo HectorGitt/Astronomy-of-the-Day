9ba78865473757f0a01b8c4dd7c932379a5d973f
Deniyi <adeniyi.olaitanhector@yahoo.com>
2025-01-02 01:58:06 +0100

    add openai controller

---
+++ utils.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI()
+
+def get_message(context):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",  # The model identifier of the model to use
+        messages=[
+            {"role": "developer", "content": [
+                {
+                    "type": "text",
+                    "text": "Write a  funny, witty, or thought-provoking caption for a daily astronomy picture using the NASA API. The caption should align with the image's theme, be engaging for a Twitter audience, and fit within 200 characters, including spaces. Use line breaks if needed for readability. Avoid using quotation marks in the captions."
+                }
+            ]},
+            {
+                "role": "user",
+                "content": context
+            }
+        ]
+    )
+    return completion.choices[0].message.content
